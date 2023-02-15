f103f65890bc049028acf31aca72b7e1355e3010
GermanBluefox <dogafox@gmail.com>
2023-02-15 23:09:16 +0100

    Formatting

--- engine/front-end/src/Components/SupportUs.js
+++ engine/front-end/src/Components/SupportUs.js
@@ -1,8 +1,6 @@
 // https://www.amazon.de/s?k=homematic&tag=httpwwwiobron-21
 // https://www.paypal.com/de_DE/DE/i/btn/btn_donateCC_LG.gif
 // http://paypal.me/iobrokerService
-// https://teespring.com/t-shirt-iobroker
-// https://teespring.com/iobroker
 
 // <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
 // <input type="hidden" name="cmd" value="_s-xclick" />
@@ -30,9 +28,7 @@ import CardMedia from '@mui/material/CardMedia';
 
 import ImgPayPal from '../assets/paypal.png';
 import ImgAmazon from '../assets/amazon.png';
-import ImgMug from '../assets/mug.png';
-import ImgMugSmall from '../assets/mug-small.png';
-import ImgTShirt from '../assets/t-shirt.png';
+import ImgAmazonSmall from '../assets/amazon_small.png';
 
 import I18n from '../i18n';
 import Utils from '../Utils';
@@ -88,8 +84,6 @@ const styles = () => ({
 });
 
 const AMAZON_LINK = 'https://www.amazon.de/s?k=homematic&tag=httpwwwiobron-21';
-const MUG_LINK = 'https://teespring.com/iobroker';
-const TSHIRT_LINK = 'https://teespring.com/t-shirt-iobroker';
 
 class SupportUs extends Component {
     constructor(props) {
@@ -152,44 +146,6 @@ class SupportUs extends Component {
         </Card>;
     }
 
-    renderMug() {
-        const { classes } = this.props;
-
-        return <Card key="mug" className={`${classes.card} ${this.props.mobile ? this.props.classes.cardMobile : ''}`}>
-            <CardActionArea onClick={() => Utils.openLink(MUG_LINK)}>
-                <CardMedia
-                    className={`${classes.media} ${classes.goodsMedia}`}
-                    image={ImgMug}
-                    title={I18n.t('Support us and buy a mug with ioBroker logo!')}
-                />
-                <CardContent>
-                    <h5>{I18n.t('Mug with logo')}</h5>
-                    <Button className={classes.amazonButton} onClick={() => Utils.openLink(MUG_LINK)}>{I18n.t('Buy a mug')}</Button>
-                    <p style={{ textAlign: 'left' }}>{I18n.t('Mug explanation')}</p>
-                </CardContent>
-            </CardActionArea>
-        </Card>;
-    }
-
-    renderTShirt() {
-        const { classes } = this.props;
-
-        return <Card key="tshirt" className={`${classes.card} ${this.props.mobile ? this.props.classes.cardMobile : ''}`}>
-            <CardActionArea onClick={() => Utils.openLink(TSHIRT_LINK)}>
-                <CardMedia
-                    className={`${classes.media} ${classes.goodsMedia}`}
-                    image={ImgTShirt}
-                    title={I18n.t('Support us and buy a t-shirt with ioBroker logo!')}
-                />
-                <CardContent>
-                    <h5>{I18n.t('T-shirt with logo')}</h5>
-                    <Button className={classes.amazonButton} onClick={() => Utils.openLink(TSHIRT_LINK)}>{I18n.t('Buy a t-shirt')}</Button>
-                    <div style={{ textAlign: 'left' }}>{ I18n.t('T-Shirt explanation') }</div>
-                </CardContent>
-            </CardActionArea>
-        </Card>;
-    }
-
     renderDialog() {
         return <Dialog
             key="dialogSupport"
@@ -208,8 +164,6 @@ class SupportUs extends Component {
                 <DialogContentText id="alert-dialog-slide-description">
                     {this.renderPaypal()}
                     {this.renderAmazon()}
-                    {this.renderMug()}
-                    {this.renderTShirt()}
                 </DialogContentText>
             </DialogContent>
             <DialogActions>
@@ -226,7 +180,7 @@ class SupportUs extends Component {
                 className={`${this.props.classes.mainButton} hue-animation`}
                 onClick={() => this.setState({ opened: true })}
             >
-                <img src={ImgMugSmall} className={this.props.classes.mainButtonIcon} alt="logo" />
+                <img src={ImgAmazonSmall} className={this.props.classes.mainButtonIcon} alt="logo" />
                 {I18n.t('Support us!')}
             </Button>,
             this.renderDialog(),
