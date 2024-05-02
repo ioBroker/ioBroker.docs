af37628e959060e23640334e2eb4cf8039f05851
GermanBluefox <dogafox@gmail.com>
2024-05-02 16:25:35 +0800

    Replace Umfrage

--- engine/front-end/src/Components/Query.js
+++ engine/front-end/src/Components/Query.js
@@ -4,9 +4,7 @@ import PropTypes from 'prop-types';
 
 import { Button } from '@mui/material';
 
-import { MdCake as IconCake } from 'react-icons/md';
-
-import UserMeeting from '../Components/UserMeeting';
+import userTreffen from '../assets/2024_05_01_usertreffen_long.jpg';
 
 const styles = () => ({
     '@keyframes hue-animation': {
@@ -35,7 +33,6 @@ const styles = () => ({
         height: 22,
         marginRight: 5,
     },
-
 });
 
 class Query extends Component {
@@ -46,30 +43,15 @@ class Query extends Component {
         };
     }
 
-    renderUserMeetingDialog() {
-        if (!this.state.showUserMeetingDialog) {
-            return null;
-        }
-        return <UserMeeting
-            key="userMeeting"
-            mobile={this.props.mobile}
-            onClose={() => this.setState({ showUserMeetingDialog: false })}
-        />;
-    }
-
     render() {
-        return [
-            <Button
-                key="button"
-                color="grey"
-                className={this.props.classes.mainButton}
-                onClick={() => this.setState({ showUserMeetingDialog: true })}
-            >
-                <IconCake className={this.props.classes.coloredIcon} />
-                Umfrage: User-Treffen November 2024
-            </Button>,
-            this.renderUserMeetingDialog(),
-        ];
+        return <Button
+            key="button"
+            color="grey"
+            className={this.props.classes.mainButton}
+            onClick={() => window.open('https://usertreffen.iobroker.in', 'usertreffen')}
+        >
+            <img src={userTreffen} alt="usertreffen"/>
+        </Button>;
     }
 }
 
