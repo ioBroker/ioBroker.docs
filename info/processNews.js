b3c4bd39f1ee8d5961f80d5046891d9353ffc399
Martin M. <mcm57@gmx.at>
2025-07-27 21:42:51 +0200

    sorting-check fixed

--- info/processNews.js
+++ info/processNews.js
@@ -4,16 +4,16 @@ const json = readFileSync(`${__dirname}/news.json`, 'utf8');
 let news;
 try {
     news = JSON.parse(json);
-    news.sort((a, b) => b.created - a.created);
+    news.sort((a, b) => Date.parse(a.created) - Date.parse(b.created));
 } catch (e) {
     throw new Error(`Cannot parse news.json: ${e}`);
 }
-if (json !== JSON.stringify(news, null, 2)) {
+if (JSON.stringify(JSON.parse(json), null, 2) !== JSON.stringify(news, null, 2)) {
     if (process.argv.includes('--check')) {
         throw new Error('News are not sorted');
     } else {
         writeFileSync('news.json', JSON.stringify(news, null, 2));
     }
+} else {
+    console.log('News are sorted')
 }
-
-
