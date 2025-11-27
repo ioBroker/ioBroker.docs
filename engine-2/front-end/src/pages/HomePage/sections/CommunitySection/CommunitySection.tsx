659e6968083616c2a1967f4b0f1bbc2cf0168c8d
GermanBluefox <dogafox@gmail.com>
2025-11-27 10:58:44 +0100

    Fixed build

--- engine-2/front-end/src/pages/HomePage/sections/CommunitySection/CommunitySection.tsx
+++ engine-2/front-end/src/pages/HomePage/sections/CommunitySection/CommunitySection.tsx
@@ -6,13 +6,13 @@ import { CustomButton } from '../../../../components/Button/Button';
 import { useForumStats } from '../../../../api/hooks/useForumStats';
 import { useTranslation } from 'react-i18next';
 
-const mainText = `/* Die Community war von Anfang an ein zentraler Bestandteil der Entwicklung von ioBroker. Obwohl das Projekt ursprünglich sprachneutral konzipiert war, hat es sich im Laufe der Zeit ergeben, dass die deutschsprachige Community am aktivsten geworden ist - vermutlich, weil ioBroker seinen Ursprung in Deutschland hat.
-
-Heute ist das ioBroker-Forum die erste Anlaufstelle für Fragen, Problemlösungen und den Austausch mit anderen Nutzern. Hier findest du eine Vielzahl an Tutorials, Skripten und Best Practices, die dir helfen, das Beste aus ioBroker herauszuholen.
-
-Neben dem Forum gibt es auch eine sehr aktive Community auf Facebook und Discord, wo täglich diskutiert, geholfen und neue Ideen geteilt werden. Egal ob Einsteiger oder Profi - hier findest du immer Unterstützung und kannst selbst dein Wissen einbringen, um anderen zu helfen. */`;
-
-const secondaryText = 'Werde Teil der Community und gestalte die Zukunft von ioBroker mit!';
+// const mainText = `/* Die Community war von Anfang an ein zentraler Bestandteil der Entwicklung von ioBroker. Obwohl das Projekt ursprünglich sprachneutral konzipiert war, hat es sich im Laufe der Zeit ergeben, dass die deutschsprachige Community am aktivsten geworden ist - vermutlich, weil ioBroker seinen Ursprung in Deutschland hat.
+//
+// Heute ist das ioBroker-Forum die erste Anlaufstelle für Fragen, Problemlösungen und den Austausch mit anderen Nutzern. Hier findest du eine Vielzahl an Tutorials, Skripten und Best Practices, die dir helfen, das Beste aus ioBroker herauszuholen.
+//
+// Neben dem Forum gibt es auch eine sehr aktive Community auf Facebook und Discord, wo täglich diskutiert, geholfen und neue Ideen geteilt werden. Egal ob Einsteiger oder Profi - hier findest du immer Unterstützung und kannst selbst dein Wissen einbringen, um anderen zu helfen. */`;
+//
+// const secondaryText = 'Werde Teil der Community und gestalte die Zukunft von ioBroker mit!';
 
 export const CommunitySection: React.FC = () => {
     const { classes } = useStyles();
